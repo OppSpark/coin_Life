59131d910ace49f15ecdf752ab34578917852564
OppSpark <picks5@naver.com>
2024-01-22 14:22:01 +0900

    페에지네이션 기능 구현중

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const port = 8088;
 
+const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
--- backend/models/free_board/freeBoardGet.js
+++ backend/models/free_board/freeBoardGet.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const path = require("path");
-const mysql = require("mysql2");
+const mysql = require("mysql");
 const mainModulePath = path.dirname(require.main.filename);
 const dbconfig = require(path.resolve(mainModulePath, "../config/dbinfo.js"));
 const connection = mysql.createConnection(dbconfig);
@@ -10,12 +10,39 @@ const connection = mysql.createConnection(dbconfig);
 router.get("/free", (req, res) => {
     const sql = "SELECT * FROM freeboard ORDER BY ID DESC;";
 
+    
     connection.query(sql, (err, rows) => {
+        console.log(rows.length)
         return res.send(rows);
-
-
     });
 });
+
+router.get("/freeSize", (req, res) => {
+    const sql = "SELECT * FROM freeboard ORDER BY ID DESC;";
+    connection.query(sql, (err, rows) => {
+        return res.send(""+ rows.length);
+    });
+});
+
+
+
+    //10 개 씩 출력
+router.get("/free/:pid", (req, res) =>{
+    const sql = "SELECT * FROM freeboard ORDER BY ID DESC LIMIT ?, ?";
+    if (req.params.pid == 1 )
+        var firstPage = 1;
+    else
+        var firstPage = ( 10 * (req.params.pid - 1) );
+
+    var lastPage = req.params.pid * 10;
+    
+    const pid = [firstPage, lastPage]
+
+    connection.query(sql, pid, (err, rows) => {
+        return res.send({rows});
+    });
+});
+
 
 router.get("/free/:id", (req, res) => {
     const sql = "SELECT * FROM freeboard WHERE id = ?";
