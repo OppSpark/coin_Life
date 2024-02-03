325093dc966355f7dd2e8577bc420f48fdf75535
OppSpark - Oracle <picks5@naevr.com>
2024-02-03 17:37:47 +0000

    페이지네이션 구현 v1.0

--- backend/models/free_board/freeBoardGet.js
+++ backend/models/free_board/freeBoardGet.js
@@ -7,15 +7,23 @@ const mainModulePath = path.dirname(require.main.filename);
 const dbconfig = require(path.resolve(mainModulePath, "../config/dbinfo.js"));
 const connection = mysql.createConnection(dbconfig);
 
+
 router.get("/free", (req, res) => {
-    const sql = "SELECT * FROM freeboard ORDER BY ID DESC;";
+    const sql = "SELECT * FROM freeboard ORDER BY ID DESC LIMIT ? OFFSET ?;";
+    let pageNo = req.query.reqPage;
 
-    connection.query(sql, (err, rows) => {
+    const offset = (pageNo - 1) * 10;
+    const params = [10, offset];
+    
+    connection.query(sql, params, (err, rows) => {
         return res.send(rows);
-
-
-    });
+    }); 
 });
+
+
+
+
+
 
 router.get("/free/:id", (req, res) => {
     const sql = "SELECT * FROM freeboard WHERE id = ?";
