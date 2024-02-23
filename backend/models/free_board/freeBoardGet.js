fc5c937a8754e28a1fe1fd1882abb5d36575aedd
OppSpark - Oracle <picks5@naevr.com>
2024-02-24 02:50:05 +0900

    v2.1.0

--- backend/models/free_board/freeBoardGet.js
+++ backend/models/free_board/freeBoardGet.js
@@ -7,31 +7,26 @@ const mainModulePath = path.dirname(require.main.filename);
 const dbconfig = require(path.resolve(mainModulePath, "../config/dbinfo.js"));
 const connection = mysql.createConnection(dbconfig);
 
-
-
 router.get("/free", (req, res) => {
     const sql = "SELECT * FROM freeboard ORDER BY ID DESC LIMIT ? OFFSET ?;";
     let pageNo = req.query.reqPage;
 
     const offset = (pageNo - 1) * 10;
     const params = [10, offset];
-    
-    connection.query(sql, params, (err, rows) => {
-    const sql = "SELECT * FROM freeboard ORDER BY ID DESC LIMIT ? OFFSET ?;";
-    let pageNo = req.query.reqPage;
 
-    const offset = (pageNo - 1) * 10;
-    const params = [10, offset];
-    
     connection.query(sql, params, (err, rows) => {
-        return res.send(rows);
-    }); 
+        const sql =
+            "SELECT * FROM freeboard ORDER BY ID DESC LIMIT ? OFFSET ?;";
+        let pageNo = req.query.reqPage;
+
+        const offset = (pageNo - 1) * 10;
+        const params = [10, offset];
+
+        connection.query(sql, params, (err, rows) => {
+            return res.send(rows);
+        });
+    });
 });
-
-
-
-
-
 
 router.get("/free/:id", (req, res) => {
     const sql = "SELECT * FROM freeboard WHERE id = ?";
